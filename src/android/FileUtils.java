dab3b3ebfcc4c27a18aa126eda8491005571e6b8
André Abascal <andreabascal@gmail.com>
2018-11-22 09:44:41 -0200

    BUFFER_SIZE=8192 test

--- src/android/FileUtils.java
+++ src/android/FileUtils.java
@@ -1081,8 +1081,8 @@ public class FileUtils extends CordovaPlugin {
             		try {
 						Log.d(PERFORMANCE_TAG,"antes do os");
                         ByteArrayOutputStream os = new ByteArrayOutputStream();
-						final int BUFFER_SIZE = 4096;
-                        // final int BUFFER_SIZE = 8192; //Padrao
+						// final int BUFFER_SIZE = 4096;
+                        final int BUFFER_SIZE = 8192; //Padrao
 						// final int BUFFER_SIZE = 16384; //Pareceu demorar mais que 8192 para 4 fotos (quando utilizado 16384 tb no LocalFileSystem).
 						// final int BUFFER_SIZE = 32768;
 						// final int BUFFER_SIZE = 65536;
